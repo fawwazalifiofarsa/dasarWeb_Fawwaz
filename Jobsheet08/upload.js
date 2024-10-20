43b3b86fe6685b0e956f81d1e817378e0611068f
fawwazalifiofarsa <fawwaz.alifio.farsa@gmail.com>
2024-10-20 18:31:50 +0700

    menghias upload file

--- Jobsheet08/upload.js
+++ Jobsheet08/upload.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
+    $("#file").change(function() {
+        if (this.files.length > 0) {
+            $("#upload-button").prop('disabled', false).css('opacity', 1);
+        } else {
+            $("#upload-button").prop('disabled', true).css('opacity', 0.5);
+        }
+    });
     $("#upload-form").submit(function(e) {
         e.preventDefault();
-
-        const formData = new FormData(this);
-
+        var formData = new FormData(this);
         $.ajax({
             type: 'POST',
             url: 'upload_ajax.php',
